4a82291d281675ede9696d5de0db112229a74326
Federico Urbani <urbanifederico5@gmail.com>
2023-01-27 22:26:01 +0100

    updated 27

---
+++ Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+
+    public static void main(String[] args) {
+
+        String a="are you able to climb, are you able to swim or are you able to fly?";
+        System.out.println(a.replaceAll("\\s(are)", "_XYZ"));
+
+
+    }
+
+}
